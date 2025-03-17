66808db0df30f79c6505db12d3977cba05a053ee
Jalogax <jalogax@gmail.com>
2025-03-17 06:54:26 -0600

    Update files

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return $this->response->setJSON(["mensaje" => "Bienvenido a CodeIgniter 4 en Vercel"]);
+        return view('welcome_message');
     }
 }
